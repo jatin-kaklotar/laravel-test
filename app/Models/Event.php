d9c065dab6a12009f938e45361f9737e46e3a615
Jatin <jatin@torinit.ca>
2022-06-15 16:51:14 +0530

    Complete the getEventsWithWorkshops function code

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    use HasFactory;
+
+    /**
+     * Get the workshops for the event .
+     */
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class, 'event_id');
+    }
 }
